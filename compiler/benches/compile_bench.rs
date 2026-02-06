8aadeb2a422304d723d39ca465c9c75a9c6f45e4
David Ifebueme <davidifebueme@gmail.com>
2026-02-06 21:01:35 +0100

    bench: add full_pipeline_erc20 benchmark

--- compiler/benches/compile_bench.rs
+++ compiler/benches/compile_bench.rs
@@ -1,5 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use pyra_compiler::{parse_from_source, program_to_abi_json, program_to_deploy_bytecode};
+use pyra_compiler::{
+    check_program, lower_program, parse_from_source, program_to_abi_json,
+    program_to_deploy_bytecode, GasReport,
+};
 
 fn compile_benchmark(c: &mut Criterion) {
     let source = include_str!("../../contracts/ERC20.pyra");
@@ -24,6 +27,18 @@ fn compile_benchmark(c: &mut Criterion) {
             let program = parse_from_source(black_box(source)).unwrap();
             let bin = program_to_deploy_bytecode(&program).unwrap();
             black_box(bin);
+        })
+    });
+
+    c.bench_function("full_pipeline_erc20", |b| {
+        b.iter(|| {
+            let program = parse_from_source(black_box(source)).unwrap();
+            let _errors = check_program(&program);
+            let _abi = program_to_abi_json(&program).unwrap();
+            let bin = program_to_deploy_bytecode(&program).unwrap();
+            let module = lower_program(&program);
+            let report = GasReport::from_module(&module);
+            black_box((bin, report));
         })
     });
 }
